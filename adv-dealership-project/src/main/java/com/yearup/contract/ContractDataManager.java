fcefa7840f0687584d137b25fb2784faffd5d4bd
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-16 14:20:11 -0500

    Added formatted output for SalesContract data in ContractDataManager

--- adv-dealership-project/src/main/java/com/yearup/contract/ContractDataManager.java
+++ adv-dealership-project/src/main/java/com/yearup/contract/ContractDataManager.java
@@ -1,5 +1,9 @@
 package com.yearup.contract;
 
+import com.yearup.dealership.Vehicle;
+
+
+
 public class ContractDataManager {
 
     public void saveContract(Contract contract) {
@@ -9,6 +13,28 @@ public class ContractDataManager {
 
         if (contract instanceof SalesContract) {
             SalesContract sale = (SalesContract) contract;
+            Vehicle v = sale.getVehicle();
+
+            line.append(String.format("SALE|%s|%s|%s|%d|%d|%s|%s|%s|%s|%d|%.2f|%.2f|%.2f|%.2f|%.2f|%s|%.2f",
+                    sale.getDate(),
+                    sale.getCustomerName(),
+                    sale.getCustomerEmail(),
+                    v.getVin(),
+                    v.getYear(),
+                    v.getMake(),
+                    v.getModel(),
+                    v.getType(),
+                    v.getColor(),
+                    v.getOdometer(),
+                    v.getPrice(),
+                    sale.getSalesTaxAmount(),
+                    sale.getRecordingFee(),
+                    sale.getProcessingFee(),
+                    sale.getTotalPrice(),
+                    sale.isFinanced() ? "YES" : "NO",
+                    sale.getMonthlyPayment()
+
+                    ));
 
         } else  if (contract instanceof LeaseContract) {
             LeaseContract lease = (LeaseContract) contract;
