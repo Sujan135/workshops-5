41db040c2860d6066015243792a2cce66da24a92
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-16 14:56:40 -0500

    Compute lease fee and ending value using price; added $100 recording fee

--- adv-dealership-project/src/main/java/com/yearup/contract/LeaseContract.java
+++ adv-dealership-project/src/main/java/com/yearup/contract/LeaseContract.java
@@ -6,14 +6,16 @@ public class LeaseContract extends Contract{
     private final double LeaseFeeRate = 0.07;
     private final double LeaseInterestRate = 0.04 / 12;
     private final int LeaseMonths = 36;
+    private final double RecordingFee = 100.00;
 
     private double expectedEndingValue;
     private  double LeaseFee;
 
     public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicle, double expectedEndingValue, double leaseFee) {
         super(date, customerName, customerEmail, vehicle);
-        this.expectedEndingValue = expectedEndingValue;
-        LeaseFee = leaseFee;
+        double price = vehicle.getPrice();
+        this.expectedEndingValue = price * 0.50 ;
+        LeaseFee = price * LeaseFeeRate;
     }
 
     public double getLeaseFeeRate() {
@@ -24,6 +26,9 @@ public class LeaseContract extends Contract{
         return expectedEndingValue;
     }
 
+    public double getRecordingFee() {
+        return RecordingFee;
+    }
 
     @Override
     public double getTotalPrice() {
